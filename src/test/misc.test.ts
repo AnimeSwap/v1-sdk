7d9184ace578b0ff9e4e0775c81c0035d323006e
zzzkky <philip.20050132@163.com>
2022-11-30 11:46:12 +0800

    v1.2.1 (#53)

--- src/test/misc.test.ts
+++ src/test/misc.test.ts
@@ -1,4 +1,5 @@
 import SDK, { NetworkType } from '../main'
+import { d } from '../utils'
 
 const address = '0x8aacfa7a452ddea2c1953e59e52be5c7171f7281ba60ea52c3d5e5950000'
 
@@ -26,50 +27,53 @@ describe('Misc Module', () => {
 
   // ---------- AutoANI ----------
 
-  // test('calculateAutoAniStakedAmount', async () => {
-  //   const address2 = '0xf920ce46ae9befa1639ef751053ce5f3de5e526df6d598ef384880faaf6eac27'
-  //   const output = await sdk.Misc.calculateAutoAniStakedAmount(address2)
-  //   console.log(output)
-  //   expect(1).toBe(1)
-  // })
+  test('calculateAutoAniStakedAmount', async () => {
+    const address2 = '0x22cc4039c75fe85472ec4817f0962abe42cd2c765691705b98314cf44d5eb49e'
+    const output = await sdk.Misc.calculateAutoAniStakedAmount(address2)
+    console.log(output)
+    expect(1).toBe(1)
+  })
 
-  // test('calculateAutoAniHarvestCallFee', async () => {
-  //   const output = await sdk.Misc.calculateAutoAniHarvestCallFee()
-  //   console.log(output)
-  //   expect(1).toBe(1)
-  // })
+  test('calculateAutoAniHarvestCallFee', async () => {
+    const output = await sdk.Misc.calculateAutoAniHarvestCallFee()
+    console.log(output)
+    expect(1).toBe(1)
+  })
 
-  // test('calculateAutoAniInfo', async () => {
-  //   const output = await sdk.Misc.calculateAutoAniInfo()
-  //   console.log(output)
-  //   expect(1).toBe(1)
-  // })
+  test('calculateAutoAniInfo', async () => {
+    const output = await sdk.Misc.calculateAutoAniInfo()
+    console.log(output)
+    expect(1).toBe(1)
+  })
 
-  // test('autoAniDepositPayload', async () => {
-  //   const payload = sdk.Misc.autoAniDepositPayload(1e8.toString())
-  //   console.log(payload)
-  //   expect(1).toBe(1)
-  // })
+  test('autoAniDepositPayload', async () => {
+    const payload = sdk.Misc.autoAniDepositPayload(1e8.toString())
+    console.log(payload)
+    expect(1).toBe(1)
+  })
 
-  // test('autoAniWithdrawAllPayload', async () => {
-  //   const payload = sdk.Misc.autoAniWithdrawAllPayload()
-  //   console.log(payload)
-  //   expect(1).toBe(1)
-  // })
+  test('autoAniWithdrawAllPayload', async () => {
+    const payload = sdk.Misc.autoAniWithdrawAllPayload()
+    console.log(payload)
+    expect(1).toBe(1)
+  })
 
-  // test('autoAniWithdrawPayload', async () => {
-  //   const address2 = '0xf920ce46ae9befa1639ef751053ce5f3de5e526df6d598ef384880faaf6eac27'
-  //   const output = await sdk.Misc.calculateAutoAniStakedAmount(address2)
-  //   const withdrawAmount = 123e8  // assume user want to withdraw 123 ANI, assert 0 < withdrawAmount < output.amount
-  //   const shares = d(withdrawAmount).mul(output.shares).div(output.amount).ceil()
-  //   const payload = sdk.Misc.autoAniWithdrawPayload(shares)
-  //   console.log(payload)
-  //   expect(1).toBe(1)
-  // })
+  test('autoAniWithdrawPayload', async () => {
+    const address2 = '0x22cc4039c75fe85472ec4817f0962abe42cd2c765691705b98314cf44d5eb49e'
+    const output = await sdk.Misc.calculateAutoAniStakedAmount(address2)
+    const withdrawAmount = 123e8  // assume user want to withdraw 123 ANI, assert 0 < withdrawAmount < output.amount
+    let shares = d(withdrawAmount).mul(output.shares).div(output.amount).ceil()
+    if (shares.gt(output.shares)) {
+      shares = output.shares
+    }
+    const payload = sdk.Misc.autoAniWithdrawPayload(shares)
+    console.log(payload)
+    expect(1).toBe(1)
+  })
 
-  // test('autoAniHarvestPayload', async () => {
-  //   const payload = sdk.Misc.autoAniHarvestPayload()
-  //   console.log(payload)
-  //   expect(1).toBe(1)
-  // })
+  test('autoAniHarvestPayload', async () => {
+    const payload = sdk.Misc.autoAniHarvestPayload()
+    console.log(payload)
+    expect(1).toBe(1)
+  })
 })
